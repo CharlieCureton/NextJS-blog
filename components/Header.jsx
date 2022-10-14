da23ec9bc21c1fa2c6e4fe2ef96f93b528e7e73f
TheIvoryCoder <ccureton21@gmail.com>
2022-10-14 07:25:01 +0100

    BlogApp

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b-4 w-full inline-block border-blue-000 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">Charlie Cureton</span>
+          </Link>
+        </div>
+        <div className="md:float-left md:contents sm: mt-5">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
